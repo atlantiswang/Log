498bbabb603e0d8893f9b0493daa1915fd0bf3dc
wanghao <atlantiswang@foxmail.com>
2019-03-25 21:56:47 +0800

    添加时间，修改由于不是二进制方式打开文件造成的换行问题

--- log.cpp
+++ log.cpp
@@ -1,6 +1,7 @@
 #include "log.h"
 #include <sys/stat.h>
 #include "lock.h"
+#include <time.h>
 
 Log* Log::m_pLogInstance = NULL;
 
@@ -71,24 +72,36 @@ bool Log::WriteLog(const char* szLog, unsigned int nLevel)
 	//写日志前的准备： 1判断当前文件大小是否超过保存量，2如果超过就备份为 m_logfile.bak(如果已经有了些文件就删除) 并创建一个新的文件m_logfile 3如果不超过就使用现在的
 	WriteLogPre();
 
+	time_t tms;
+	time(&tms);
+
+	struct tm stm;
+	localtime_s(&stm, &tms);
+	char szTime[128] = { 0 };
+	sprintf_s(szTime, "[%d/%d/%d:%d/%d/%d]", stm.tm_year+1900, stm.tm_mon+1, stm.tm_mday,
+		stm.tm_hour, stm.tm_min, stm.tm_sec);
+
+	m_strLogInfo = szTime;
+
 	if (nLevel == ERROR_LEVEL)
 	{
-		m_strLogInfo = "ERROR:";
+		m_strLogInfo += "[ERROR]:";
 	}
 	else if (nLevel == WARN_LEVEL)
 	{
-		m_strLogInfo = "WARNING:";
+		m_strLogInfo += "[WARNING]:";
 	}
 	else if (nLevel == INFO_LEVE)
 	{
-		m_strLogInfo = "INFO:";
+		m_strLogInfo += "[INFO]:";
 	}
 
 	m_strLogInfo += szLog;
 	m_strLogInfo += "\r\n";
 
 	FILE *fp = NULL;
-	fopen_s(&fp, m_logfile.c_str(), "a");
+	//一定要以二进制方式打开
+	fopen_s(&fp, m_logfile.c_str(), "ab");
 	if (fp != NULL)
 	{
 		fwrite(m_strLogInfo.c_str(), 1, m_strLogInfo.size(), fp);
